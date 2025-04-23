49ae2598ea3529510dda29e48f03e388091e06cd
Chetan More <140832286+chetan21122004@users.noreply.github.com>
2025-04-23 23:56:57 +0530

    reReDeploy

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center justify-center">
-      <div className="flex h-10 w-10 items-center justify-center rounded-md bg-emerald-500 text-white font-bold">
+      <div className="flex p-1 items-center justify-center rounded-md bg-emerald-500 text-white font-bold">
         VisionCraft
       </div>
     </Link>
